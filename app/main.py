ae6bdc4b31f776a844c75361fb04e7a95ee7e80f
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-03-24 15:23:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    temp = {}
+    result = []
+    for person_data in people:
+        person = Person(person_data["name"], person_data["age"])
+        temp[person_data["name"]] = person
+        result.append(person)
+
+    for person_data, person in zip(people, result):
+        if person_data.get("wife") is not None:
+            person.wife = temp[person_data["wife"]]
+        if person_data.get("husband") is not None:
+            person.husband = temp[person_data["husband"]]
+    return result
